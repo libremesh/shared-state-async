b08c825e01e465732a42cb47fec9a6ff42bb387d
javier.jorge <javier.jorge@unc.edu.ar>
2023-02-22 11:01:44 -0300

    missing changes from prev commit

--- tests/mergeechotest.cc
+++ tests/mergeechotest.cc
@@ -1,7 +1,30 @@
+/*
+ * Shared State
+ *
+ * Copyright (c) 2023  Javier Jorge <jjorge@inti.gob.ar>
+ * Copyright (c) 2023  Instituto Nacional de Tecnología Industrial
+ * Copyright (C) 2023  Asociación Civil Altermundi <info@altermundi.net>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 #include "doctest/doctest.h"
 #include "sharedstate.hh"
 #include "shared_state_error_code.hh"
 #include "piped_async_command.hh"
+#include "debug/rsdebuglevel2.h"
 
 // Tests that don't naturally fit in the headers/.cpp files directly
 // can be placed in a tests/*.cpp file. Integration tests are a good example.
@@ -10,7 +33,7 @@ TEST_CASE("returnmerge")
 {
   std::string original = "mensajeaverificar";
   std::string merged = SharedState::mergestate(original);
-  std::cout << merged << original << "--------------------------------------------------------------"<< std::endl;
+  std::cout << merged << original << "--------------------------------------------------------------" << std::endl;
   CHECK(original.size() == merged.size());
   CHECK(original == merged);
 }
@@ -24,8 +47,6 @@ TEST_CASE("parametrizedmerge")
   CHECK(original.size() == merged.size());
   CHECK(original == merged);
 }
-
-
 
 void verificar(std::string original)
 {
@@ -50,7 +71,7 @@ void verificarExpected(std::string original)
 
 void verificarExpectedWillFail(std::string original)
 {
-  auto merged = SharedState::expMergestate(original,true);
+  auto merged = SharedState::expMergestate(original, true);
   CHECK_FALSE(merged);
   std::error_condition(SharedState::SharedStateErrorCode::OpenPipeError);
   CHECK(merged.error().message() == make_error_condition(SharedState::SharedStateErrorCode::OpenPipeError).message());
@@ -67,7 +88,7 @@ void verificarExpectedWillFail(std::string original)
 //   std::cout << merged << " --- " << original;
 //   CHECK(original.size() == merged.size());
 //   CHECK(original == merged);
- 
+
 // }
 
 TEST_CASE("Opt merge")
@@ -76,18 +97,19 @@ TEST_CASE("Opt merge")
   verificarOptional(original);
 }
 
-TEST_CASE("Parametrized merge test") {
-    std::vector<std::string> data {"","mensajeaverificar", "asdasdaasd > saddsdfsdf","546654654654","546654654654546654654654546654654654546654654654" };
+TEST_CASE("Parametrized merge test")
+{
+  std::vector<std::string> data{"", "mensajeaverificar", "asdasdaasd > saddsdfsdf", "546654654654", "546654654654546654654654546654654654546654654654"};
 
-    for(auto& i : data) {
-        CAPTURE(i); // log the current input data
-        verificarOptional(i);
-        verificar(i);
-        verificarExpected(i);
-        verificarExpectedWillFail(i);
-        //verificarPiped(i);
-    }
+  for (auto &i : data)
+  {
+    CAPTURE(i); // log the current input data
+    verificarOptional(i);
+    verificar(i);
+    verificarExpected(i);
+    verificarExpectedWillFail(i);
+    // verificarPiped(i);
+  }
 }
 
 // sync vacio devuelve lo mismo que get
- 
